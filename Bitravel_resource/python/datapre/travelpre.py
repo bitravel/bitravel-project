27c98987b5c60a55d735accbcda84a68c24147d4
spacekay <bethjkim0311@gmail.com>
2021-11-03 14:10:22 +0900

    made some columns

--- Bitravel_resource/python/datapre/travelpre.py
+++ Bitravel_resource/python/datapre/travelpre.py
@@ -5,20 +5,22 @@ import numpy as np
 # xlrd
 
 
-def get_metro(address):
-    front = address[0:2]
-    if address.startswith("경상") or address.startswith('전라') or address.startswith("충청"):
-        addition = address[0:1]+address[3:4]
-        return addition
-    else:
-        return front
-
-
 travel_df = pd.read_excel('../travelspots/2021113_1.xls')
 travel_df = travel_df.drop(["우편번호", "관리자", " 유산구분", "개장일", "쉬는날", "체험안내", "체험가능연령", "수용인원", "이용시기", "이용시간"], axis=1)
-travel_df["광역지자체"] = travel_df["주소"].str.slice(start=0, stop=3)
+tmp_list = travel_df["주소"].str.split(' ')
+travel_df["광역지자체"] = tmp_list.str[0]
+travel_df["기초지자체"] = tmp_list.str[1]
+travel_df["광역지자체"] = travel_df["광역지자체"].replace('경상북도', '경북')
+travel_df["광역지자체"] = travel_df["광역지자체"].replace('경상남도', '경남')
+travel_df["광역지자체"] = travel_df["광역지자체"].replace('전라북도', '전북')
+travel_df["광역지자체"] = travel_df["광역지자체"].replace('전라남도', '전남')
+travel_df["광역지자체"] = travel_df["광역지자체"].replace('충청북도', '충북')
+travel_df["광역지자체"] = travel_df["광역지자체"].replace('충청남도', '충남')
+travel_df["광역지자체"] = travel_df["광역지자체"].str.slice(start=0, stop=2)
 travel_df.replace(np.NaN, 'NULL', inplace=True)
-print(travel_df['광역지자체'])
+travel_df["중분류"] = 'NULL'
+travel_df["소분류"] = 'NULL'
+print(travel_df[["광역지자체", "기초지자체", "소분류"]])
 
 #travel = travel_df.to_dict()
 
