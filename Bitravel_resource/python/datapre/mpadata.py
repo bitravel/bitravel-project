c3e181fb6f2703772846ab2419fb5119023f46d1
spacekay <bethjkim0311@gmail.com>
2021-11-03 17:06:29 +0900

    반복문 사용을 통한 Kakao API 호출

--- Bitravel_resource/python/datapre/mpadata.py
+++ Bitravel_resource/python/datapre/mpadata.py
@@ -9,12 +9,18 @@ popular_df = pd.read_excel('../popularspots/지역별 관광지 검색 순위_20
 new = popular_df['관광지명']
 print(new)
 
-driver = webdriver.Chrome()
-driver.get("https://map.kakao.com/")
-elem = driver.find_element_by_name("q")
-elem.send_keys(str(new[10]))
-elem.send_keys(Keys.RETURN)
-driver.implicitly_wait(time_to_wait=5)
-print(driver.find_element_by_xpath('//*[@id="info.search.place.list"]/li[1]/div[5]/div[2]/p[1]').text)
+address = []
 
+for i in range(0, 5):
+    driver = webdriver.Chrome()
+    driver.get("https://map.kakao.com/")
+    elem = driver.find_element_by_name("q")
+    elem.send_keys(str(new[i]))
+    elem.send_keys(Keys.RETURN)
+    driver.implicitly_wait(time_to_wait=5)
+    address.append(driver.find_element_by_xpath('//*[@id="info.search.place.list"]/li[1]/div[5]/div[2]/p[1]').text)
+    print(address[i])
+    i = i+1
+
+print(address)
 
