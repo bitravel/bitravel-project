8846b9248de6d8594dd0ea00f23432812d70771f
spacekay <bethjkim0311@gmail.com>
2021-11-03 16:59:10 +0900

    applied Kakaomap API

--- Bitravel_resource/python/datapre/mpadata.py
+++ Bitravel_resource/python/datapre/mpadata.py
@@ -5,18 +5,16 @@ from selenium.webdriver.common.keys import Keys
 import pandas as pd
 import numpy as np
 
-'''
-travel_df = pd.read_excel('./유형별 인기 관광지/지역별 관광지 검색 순위_20211103_제주자연.xlsx')
-new = travel_df[{'관광지명'}]
+popular_df = pd.read_excel('../popularspots/지역별 관광지 검색 순위_20211103_제주자연.xlsx')
+new = popular_df['관광지명']
 print(new)
-'''
 
 driver = webdriver.Chrome()
 driver.get("https://map.kakao.com/")
 elem = driver.find_element_by_name("q")
-elem.send_keys('협재해수욕장')
+elem.send_keys(str(new[10]))
 elem.send_keys(Keys.RETURN)
-driver.implicitly_wait(time_to_wait=3)
+driver.implicitly_wait(time_to_wait=5)
 print(driver.find_element_by_xpath('//*[@id="info.search.place.list"]/li[1]/div[5]/div[2]/p[1]').text)
 
 
