1fd17265de1fd0debf45fa50713ead367bf59d18
spacekay <bethjkim0311@gmail.com>
2021-11-05 17:01:28 +0900

    added some comments

--- Bitravel_resource/python/datapre/averagecount.py
+++ Bitravel_resource/python/datapre/averagecount.py
@@ -10,17 +10,9 @@ attr = ['자연관광지', '역사관광지', '휴양관광지', '문화시설',
 for i in range(1, 5):
     path = '../searchcount/관광지 유형별 검색건수' + str(i) + '.xlsx'
     search_df = pd.read_excel(path)
-    search_df = search_df.drop(['년', '전체', '공연/행사', '레포츠(육상/해상/항공)', '쇼핑'], axis=1)
-    j = -1
-    month = ""
-    for index, row in search_df.iterrows():
-        j = j + 1
-        if not np.isnan(row['월']):
-            month = search_df.loc[j, '월']
-        else:
-            search_df.loc[j, '월'] = month
+    search_df = search_df.drop(['년', '월', '전체', '공연/행사', '레포츠(육상/해상/항공)', '쇼핑'], axis=1)
+
     result_df = pd.DataFrame(search_df)
-    result_df = result_df.drop(['월'], axis=1)
     result_df = result_df.drop(result_df.index[0:])
 
     df_list = []
@@ -28,12 +20,17 @@ for i in range(1, 5):
     if i == 4:
         length = 3
     for j in range(0, length):
+        # 각 지자체별 행을 모두 뽑아서 하나의 dataframe에 넣고, 그것을 df_list의 원소로 추가한다.
         df_list.append(search_df[(search_df['광역지자체'] == local_list[(i - 1) * 5 + j])])
+
+        # 각 리스트의 Attribute 별로 평균을 구하여 한 줄 짜리 dataframe으로 만든 뒤, result_df의 맨 끝에 추가한다.
         result_df.loc[len(result_df)] = [local_list[(i - 1) * 5 + j], df_list[j][attr[0]].mean(),
                                          df_list[j][attr[1]].mean(),
                                          df_list[j][attr[2]].mean(),
                                          df_list[j][attr[3]].mean(), df_list[j][attr[4]].mean()]
+    # 잘 추가되었는지 확인
     print(result_df)
+    
     if i == 1:
         result_df.to_csv('../preprocessing/search_average.csv', sep=',', mode='w', header=True, index=False,
                          encoding='cp949')
