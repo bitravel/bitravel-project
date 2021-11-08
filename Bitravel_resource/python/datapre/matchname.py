3b461f5f2518b0912f991e168f7fa65b1df93b62
spacekay <bethjkim0311@gmail.com>
2021-11-08 16:58:09 +0900

    changed separator , to @

--- Bitravel_resource/python/datapre/matchname.py
+++ Bitravel_resource/python/datapre/matchname.py
@@ -31,7 +31,8 @@ for i in range(0, len(new)):
         new_index = []
         for i_num in index:
             pa_index = db_df.loc[i_num, '명칭'].find('(')
-            if category_df['상세주소'].iloc[i].find(db_df['기초지자체'].iloc[i_num]) != -1 and len(db_df.loc[i_num, '명칭'][0:pa_index]) < len(new[i])+3:
+            if category_df['상세주소'].iloc[i].find(db_df['기초지자체'].iloc[i_num]) != -1 and len(
+                    db_df.loc[i_num, '명칭'][0:pa_index]) < len(new[i]) + 3:
                 new_index.append(i_num)
         if len(new_index) == 1:
             db_df.loc[new_index, '중분류'] = category_df.loc[i, '중분류 카테고리']
@@ -41,5 +42,4 @@ for i in range(0, len(new)):
             print(new[i])
     print(t)
 
-db_df.to_csv('../preprocessing/travel_after.csv', sep=',', mode='w', header=True, index=False, encoding='cp949')
-
+db_df.to_csv('../preprocessing/travel_after.csv', sep='@', mode='w', header=True, index=False, encoding='cp949')
