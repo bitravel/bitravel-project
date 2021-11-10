8e92d3779d04f45cf814b8e1bf5952ab83f68420
spacekay <bethjkim0311@gmail.com>
2021-11-10 10:09:10 +0900

    edited the user entity class

--- Bitravel/src/main/java/com/bitravel/data/entity/User.java
+++ Bitravel/src/main/java/com/bitravel/data/entity/User.java
@@ -4,10 +4,10 @@ import java.sql.Timestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -22,16 +22,33 @@ public class User {
 	// 초안 기준 1차 설계 진행중
  
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private @Column(name = "userId") Long userId;
+    private @Column(name = "userId") String userId;
+    
+    @NotNull
+    @Size(min=2, max=15)
     private @Column(name = "userName") String userName;
+    
+    @NotNull
+    @Size(min=2, max=15)
     private @Column(name = "nickname") String nickname;
+    
+    @NotNull
     private @Column(name = "email") String email;
+    
+    @NotNull
     private @Column(name = "pwd") String password;
-    private @Column(name = "point") Long point;
+    
+    @Column(name = "point", columnDefinition = "int default 0")
+    private Long point;
+    
+    @NotNull
     private @Column(name = "gender") String gender;
+    
+    @NotNull
+    @Size(min=1, max=3)
     private @Column(name = "age") Integer age;
     
+    @NotNull
     @CreationTimestamp
     private @Column(name = "userDate") Timestamp userDate;
      
