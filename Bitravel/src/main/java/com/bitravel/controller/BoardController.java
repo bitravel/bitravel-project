eddbac2a7066515cd02fa0a9a3ffe12f5903c2ed
spacekay <bethjkim0311@gmail.com>
2021-11-10 16:35:59 +0900

    Board Controller 내용 보충

--- Bitravel/src/main/java/com/bitravel/controller/BoardController.java
+++ Bitravel/src/main/java/com/bitravel/controller/BoardController.java
@@ -3,6 +3,8 @@ package com.bitravel.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,20 +32,27 @@ public class BoardController {
     
     // DB 연결 테스트용 링크 -> 포트번호만 상황에 따라 바꾸어서 사용하세요. http://localhost:8080/swagger-ui/
      
- 
-    @RequestMapping(value = "", method = RequestMethod.GET)
+    
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
     @ApiOperation(value = "글 목록 조회", notes = "글 목록을 조회하는 API.")
     public List<Board> getBoardList(){
         return boardService.selectBoardList();
     }
  
-    @RequestMapping(value = "/{bid}", method = RequestMethod.GET)
+    @RequestMapping(value = "/view/{bid}", method = RequestMethod.GET)
     @ApiOperation(value = "글 내용 조회", notes = "개별 글의 정보를 조회하는 API. Board entity 클래스의 bid값을 기준으로 데이터를 가져온다.")
     public Optional<Board> getBoard( @PathVariable("bid") Long bid ){
         return boardService.selectBoard(bid);
     }
- 
-    @RequestMapping(value = "", method = RequestMethod.POST)
+    
+    // 다른 method들도 이후 view(template)가 완성되고 나면 return을 string으로 바꾸어 준 뒤, Session에 생성한 객체를 담는 방향으로 모두 변경 예정
+    @RequestMapping(value = "/writeform")
+    public String showWriteForm() {
+    	return "writeform";
+    }
+    // 글쓰기 페이지에서 user attribute를 session에서 꺼내오면 됨
+    
+    @RequestMapping(value = "/write", method = RequestMethod.POST)
     @ApiOperation(value = "글 작성", notes = "글 내용을 저장하는 API. Board entity 클래스로 데이터를 저장한다.")
     public ResponseEntity<ApiResponseMessage> insertBoard( Board board ){
         ApiResponseMessage message = new ApiResponseMessage("Success", "등록되었습니다.", "", "");
@@ -60,7 +69,7 @@ public class BoardController {
         return response;
     }
      
-    @RequestMapping(value = "", method = RequestMethod.PUT)
+    @RequestMapping(value = "/modify", method = RequestMethod.PUT)
     @ApiOperation(value = "글 수정", notes = "글 내용을 수정하는 API. Board entity 클래스로 데이터를 수정한다.<br>이때엔 정보를 등록할 때와는 다르게 bid 값을 함깨 보내줘야한다.")
     public ResponseEntity<ApiResponseMessage> updateBoard( Board board ){
         ApiResponseMessage message = new ApiResponseMessage("Success", "등록되었습니다.", "", "");
@@ -75,8 +84,13 @@ public class BoardController {
          
         return response;
     }
- 
-    @RequestMapping(value = "/{bid}", method = RequestMethod.DELETE)
+    
+    @RequestMapping(value = "/modifyform")
+    public String showModifyForm() {
+    	return "modifyform";
+    }
+    
+    @RequestMapping(value = "/delete/{bid}", method = RequestMethod.DELETE)
     @ApiOperation(value = "글 삭제", notes = "글 내용을 삭제하는 API. Board entity 클래스의 bid 값으로 데이터를 삭제한다.")
     public ResponseEntity<ApiResponseMessage> deleteBoard( @PathVariable("bid") Long bid ){
         ApiResponseMessage message = new ApiResponseMessage("Success", "등록되었습니다.", "", "");
