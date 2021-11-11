d333cc8509d3707bfe656a0be5c22913e2f1d223
spacekay <bethjkim0311@gmail.com>
2021-11-11 18:18:20 +0900

    added component scan

--- Bitravel/src/main/java/com/bitravel/BitravelApplication.java
+++ Bitravel/src/main/java/com/bitravel/BitravelApplication.java
@@ -2,8 +2,11 @@ package com.bitravel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@ComponentScan(basePackages= {"com.bitravel"})
 public class BitravelApplication {
 
 	public static void main(String[] args) {
