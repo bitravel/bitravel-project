3dfeecc9ee0f3100c510261cc9652803d9859009
spacekay <bethjkim0311@gmail.com>
2021-11-12 17:28:23 +0900

    Dto class files (임시 보관용 data bean)

---
+++ Bitravel/src/main/java/com/bitravel/data/dto/UserDto.java
@@ -0,0 +1,47 @@
+package com.bitravel.data.dto;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+	
+	@NotNull
+	@Size(min=5, max=50)
+	private String email;
+	
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	@NotNull
+    @Size(min=4, max=100)
+	private String password;
+	
+	@NotNull
+	@Size(min=2, max=20)
+	private String nickname;
+	
+	@NotNull
+	@Size(min=2, max=20)
+	private String realname;
+	
+	@NotNull
+	private Integer point;
+	
+	@NotNull
+	private String gender;
+	
+	@NotNull
+	private Integer age;
+	
+}
