07492a74a6bd84d272e5d5fa5f41d13c9ceb2b04
spacekay <bethjkim0311@gmail.com>
2021-11-12 17:29:51 +0900

    Authority 인증 방식 변경으로 인한 repository 수정

--- Bitravel/src/main/java/com/bitravel/data/repository/UserRepository.java
+++ Bitravel/src/main/java/com/bitravel/data/repository/UserRepository.java
@@ -1,13 +1,14 @@
 package com.bitravel.data.repository;
 
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bitravel.data.entity.User;
 
 // Entity가 하나 추가될 때마다 별개의 Repository class가 필요함
-public interface UserRepository extends CrudRepository<User, String>{
-	@Query("select u from User u where u.userId = :id")
-	User findUser(@Param("id") String inputid);
+public interface UserRepository extends JpaRepository<User, Long>{
+	@EntityGraph(attributePaths = "authorities")
+	Optional<User> findOneWithAuthoritiesByEmail(String email);
 }
