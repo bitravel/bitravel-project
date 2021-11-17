058f2096d417edef6d1b5c1eb644006fb5fabea9
spacekay <bethjkim0311@gmail.com>
2021-11-17 16:30:34 +0900

    Board Comment Dto

---
+++ Bitravel/src/main/java/com/bitravel/data/dto/BoardCommentRequestDto.java
@@ -0,0 +1,38 @@
+package com.bitravel.data.dto;
+
+import com.bitravel.data.entity.Board;
+import com.bitravel.data.entity.BoardComment;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor (access = AccessLevel.PROTECTED)
+public class BoardCommentRequestDto {
+	
+	private Long boardId; // 원글 번호
+	private String userEmail; // 작성자 (이메일)
+	private String commentContent; // 내용
+	private Integer commentLevel; // 댓글 레벨 (대댓글 여부 확인)
+	
+    @JsonIgnore
+    private Board board;
+	
+	public BoardComment toEntity() {
+		return BoardComment.builder()
+				.board(board)
+				.commentContent(commentContent)
+				.userEmail(userEmail)
+				.commentLevel(commentLevel)
+				.build();
+	}
+	
+	public void setUserEmail(String email) {
+		this.userEmail = email;
+	}
+	
+}
---
+++ Bitravel/src/main/java/com/bitravel/data/dto/BoardCommentResponseDto.java
@@ -0,0 +1,34 @@
+package com.bitravel.data.dto;
+
+import com.bitravel.data.entity.Board;
+import com.bitravel.data.entity.BoardComment;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor (access = AccessLevel.PROTECTED)
+public class BoardCommentResponseDto {
+	
+	private Long bCommentId;
+	private String userEmail; // 작성자 (이메일)
+	private String commentContent; // 내용
+	private Integer commentLevel; // 댓글 레벨 (대댓글 여부 확인)
+	private Long boardId;
+	
+	public BoardCommentResponseDto (BoardComment bComment) {
+		this.bCommentId = bComment.getBCommentId();
+		this.boardId = bComment.getBoard().getBoardId();
+		this.commentContent = bComment.getBCommentContent();
+		this.userEmail = bComment.getUserEmail();
+		this.commentLevel = bComment.getBCommentLevel();
+	}
+	
+	public void setUserEmail(String email) {
+		this.userEmail = email;
+	}
+	
+}
