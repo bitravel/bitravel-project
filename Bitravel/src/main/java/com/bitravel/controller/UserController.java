1e3d20796e82f11bec479490a44ffaa61fd7f875
spacekay <bethjkim0311@gmail.com>
2021-11-17 11:09:43 +0900

    회원관리 추가기능 API 구현 확인

--- Bitravel/src/main/java/com/bitravel/controller/UserController.java
+++ Bitravel/src/main/java/com/bitravel/controller/UserController.java
@@ -1,20 +1,14 @@
 package com.bitravel.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,10 +16,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.bitravel.data.dto.LoginDto;
+import com.bitravel.data.dto.TokenDto;
 import com.bitravel.data.dto.UserDto;
 import com.bitravel.data.entity.User;
 import com.bitravel.jwt.JwtFilter;
-import com.bitravel.jwt.TokenProvider;
 import com.bitravel.service.UserService;
 
 import io.swagger.annotations.Api;
@@ -40,46 +34,28 @@ import lombok.extern.slf4j.Slf4j;
 public class UserController {
 
 	private final UserService userService;
-	private final TokenProvider tokenProvider;
-	private final AuthenticationManagerBuilder authenticationManagerBuilder;
-	private final StringRedisTemplate redisTemplate;
 
 	@PostMapping("/login")
 	public String login(@Valid LoginDto loginDto, HttpServletResponse response) {
-		UsernamePasswordAuthenticationToken authenticationToken = 
-				new UsernamePasswordAuthenticationToken(loginDto.getEmail(), loginDto.getPassword());		
-		Authentication authentication;
-		try {
-			authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-		} catch (BadCredentialsException e) {
+		TokenDto tokenInfo = userService.getUserAuthentication(loginDto);
+		if(tokenInfo==null) {
 			return "loginPage";
 		}
-		SecurityContextHolder.getContext().setAuthentication(authentication);
-		String jwt = tokenProvider.createToken(authentication);
-		//HttpHeaders httpheaders = new HttpHeaders();
-		//httpheaders.setConnection("http://localhost:8080");
-		// httpheaders.add(JwtFilter.AUTHORIZATION_HEADER, jwt);
+		String jwt = tokenInfo.getToken();
 		response.setHeader(JwtFilter.AUTHORIZATION_HEADER, "Bearer "+jwt);
 		return "index";
 	}
 
 	@PostMapping("/logout")
 	public String logout (HttpServletRequest request, HttpServletResponse response) {
-		log.info("진입 완료");
-
 		String jwt = response.getHeader(JwtFilter.AUTHORIZATION_HEADER);
-		if(StringUtils.hasText(jwt) && jwt.startsWith("Bearer ")) {jwt = jwt.substring(7); }
-		else
-			return "index"; // 어차피 내부든 쿠키든 저장이 안되므로 소용없음 다시 리셋됨
-		log.info(jwt); ValueOperations<String, String>
-		logoutValueOperations = redisTemplate.opsForValue();
-		logoutValueOperations.set(jwt, jwt); User user = (User)
-				tokenProvider.getAuthentication(jwt).getPrincipal();
-		log.info("로그아웃 유저 아이디 : '{}', 유저 이름 : '{}'", user.getEmail(),
-				user.getNickname());
-		
-		// 실제 구현 완료하고 나면 반드시 redirect해야 함
-		return "redirect:/";
+		log.info(jwt);
+		if(userService.CancelUserAuthentication(jwt)) {
+			// 실제 구현 완료하고 나면 반드시 redirect해야 함
+			return "redirect:/";
+		} else {
+			return "index";
+		}
 	}
 
 	@PostMapping("/signup")
@@ -90,14 +66,34 @@ public class UserController {
 	}
 
 	@GetMapping("/user")
-	@PreAuthorize("hasAnyRole('ROLE_USER','ROLE_ADMIN')")
-	public ResponseEntity<User> getMyUserInfo() {
+	//@PreAuthorize("hasAnyRole('ROLE_USER','ROLE_ADMIN')")
+	public ResponseEntity<UserDto> myUserInfo() {
 		return ResponseEntity.ok(userService.getMyUserWithAuthorities().get());
 	}
 
 	//@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
 	@GetMapping("/user/{uid}")
-	public ResponseEntity<User> getUserInfo(@PathVariable("uid") Long uid) {
-		return ResponseEntity.ok(userService.selectUser(uid).get());
+	public ResponseEntity<UserDto> userInfoById(@PathVariable("uid") Long uid) {
+		return ResponseEntity.ok(userService.getAnyUserById(uid).get());
+	}
+	
+	@GetMapping("/user/list")
+	public ResponseEntity<List<UserDto>> allUserList() {
+		return ResponseEntity.ok(userService.getAllUserList());
+	}
+	
+	@PostMapping("/user/modify")
+	public ResponseEntity<User> modifyUser (UserDto userDto) {
+		return ResponseEntity.ok(userService.updateUser(userDto));
+	}
+	
+	@PostMapping("/user/modifyPassword")
+	public ResponseEntity<String> modifyUserPassword (String email, String password) {
+		return ResponseEntity.ok(userService.updateUserPassword(email, password));
+	}
+	
+	@PostMapping("user/delete")
+	public ResponseEntity<Boolean> deleteUser(String email) {
+		return ResponseEntity.ok(userService.deleteUser(email));
 	}
 }
--- Bitravel/src/main/java/com/bitravel/service/UserService.java
+++ Bitravel/src/main/java/com/bitravel/service/UserService.java
@@ -1,30 +1,86 @@
 package com.bitravel.service;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
+import com.bitravel.data.dto.LoginDto;
+import com.bitravel.data.dto.TokenDto;
 import com.bitravel.data.dto.UserDto;
 import com.bitravel.data.entity.Authority;
 import com.bitravel.data.entity.User;
 import com.bitravel.data.repository.UserRepository;
-import com.bitravel.util.SecurityUtil;
+import com.bitravel.jwt.TokenProvider;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class UserService {
-    
+
 	private UserRepository userRepository;
 	private PasswordEncoder passwordEncoder;
-	
+	private final TokenProvider tokenProvider;
+	private final AuthenticationManagerBuilder authenticationManagerBuilder;
+	private final StringRedisTemplate redisTemplate;	
+
+	// 로그인
+	@Transactional(readOnly = true, isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
+	public TokenDto getUserAuthentication(LoginDto loginDto) {
+		UsernamePasswordAuthenticationToken authenticationToken = 
+				new UsernamePasswordAuthenticationToken(loginDto.getEmail(), loginDto.getPassword());		
+		Authentication authentication;
+		try {
+			authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+		} catch (BadCredentialsException e) {
+			return null;
+		}
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		String jwt = tokenProvider.createToken(authentication);
+		return new TokenDto(jwt);
+	}
+
+	// 로그아웃
 	@Transactional
+	public boolean CancelUserAuthentication (String jwt) {
+		// 어차피 내부든 쿠키든 저장이 안되므로 소용없음 다시 리셋됨 (11월 17일 현재 상황 기준)
+		if(StringUtils.hasText(jwt) && jwt.startsWith("Bearer "))
+			jwt = jwt.substring(7);
+		else
+			return false;		
+		try {
+			ValueOperations<String, String> logoutValueOperations = redisTemplate.opsForValue();
+			logoutValueOperations.set(jwt, jwt);
+			User user = (User) tokenProvider.getAuthentication(jwt).getPrincipal();
+			log.info("로그아웃 유저 아이디 : '{}', 유저 이름 : '{}'", user.getEmail(),	user.getNickname());
+		} catch(Exception e) {
+			log.debug("error: "+e);
+			return false;
+		}
+		return true;
+	}
+
+	// 회원가입
+	@Transactional(readOnly = false, isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
 	public User signup(UserDto userDto) {
 		if(userRepository.findOneWithAuthoritiesByEmail(userDto.getEmail()).orElse(null) != null) {
 			throw new RuntimeException("이미 가입되어 있는 회원입니다.");
@@ -36,10 +92,12 @@ public class UserService {
 					.build();
 		} else {
 			authority = Authority.builder()
-				.roleName("ROLE_USER")
-				.build();
+					.roleName("ROLE_USER")
+					.build();
 		}
-		
+
+		log.info(userDto.getPassword());
+
 		User user = User.builder()
 				.email(userDto.getEmail())
 				.password(passwordEncoder.encode(userDto.getPassword()))
@@ -53,19 +111,77 @@ public class UserService {
 				.build();
 		return userRepository.save(user);
 	}
-	
-	@Transactional(readOnly = true)
+
+	// 이메일(PK)로 찾기
+	@Transactional(readOnly = true, isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
 	public Optional<User> getUserWithAuthorities(String email) {
 		return userRepository.findOneWithAuthoritiesByEmail(email);
 	}
-	
-	@Transactional(readOnly = true)
-	public Optional<User> getMyUserWithAuthorities() {
-		return SecurityUtil.getCurrentEmail().flatMap(userRepository::findOneWithAuthoritiesByEmail);
-	}
-	
+
+	// 자기 자신 (로그인된 정보) 불러오기
 	@Transactional(readOnly = true, isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
-	public Optional<User> selectUser(Long id) {
-		return userRepository.findById(id);
+	public Optional<UserDto> getMyUserWithAuthorities() {
+		log.debug("here?");
+		//return SecurityUtil.getCurrentEmail().flatMap(userRepository::findOneWithAuthoritiesByEmail);
+		// Service logic 점검용
+		return userRepository.findOneWithAuthoritiesByEmail(Optional.ofNullable("admin").get()).map(UserDto::new);
+	}
+
+	// 아이디(회원 일련번호)로 찾기
+	@Transactional(readOnly = true, isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
+	public Optional<UserDto> getAnyUserById(Long id) {
+		Optional<User> user = userRepository.findById(id);
+		return user.map(UserDto::new);
+	}
+
+	// 회원 전체 리스트 불러오기
+	@Transactional(readOnly = true, isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
+	public List<UserDto> getAllUserList() {
+		Sort sort = Sort.by(Direction.ASC, "userId");
+		List<User> list = userRepository.findAll(sort);
+		return list.stream().map(UserDto::new).collect(Collectors.toList());
+	}
+
+	// 회원정보 입력받은 내용으로 수정하기
+	@Transactional(readOnly = false, isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
+	public User updateUser(UserDto userDto) {
+		Optional<User> userTemp = userRepository.findOneWithAuthoritiesByEmail(userDto.getEmail());
+		if(userTemp.isEmpty()) {
+			return null;
+		}
+		User user = userTemp.get();
+		user.setEmail(userDto.getEmail());
+		user.setNickname(userDto.getNickname());
+		user.setPoint(userDto.getPoint());
+		// View 구현할 때 일반 유저에게 수정을 허용할 내용과 그렇지 않은 내용을 구별해야 함
+		user.setGender(userDto.getGender());
+		user.setAge(userDto.getAge());
+		userRepository.save(user);
+		return user;
+	}
+
+	// 비밀번호만 수정하기
+	@Transactional(readOnly = false, isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
+	public String updateUserPassword(String email, String password) {
+		Optional<User> userTemp = userRepository.findOneWithAuthoritiesByEmail(email);
+		if(userTemp.isEmpty()) {
+			return null;
+		}
+		User user = userTemp.get();
+		user.setPassword(passwordEncoder.encode(password));
+		userRepository.save(user);
+		return user.getPassword();
+	}
+
+	// 회원정보 삭제하기
+	@Transactional(readOnly = false, isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
+	public boolean deleteUser(String email) {
+		Optional<User> userTemp = userRepository.findOneWithAuthoritiesByEmail(email);
+		if(userTemp.isEmpty()) {
+			return false;
+		}
+		User user = userTemp.get();
+		userRepository.deleteById(user.getUserId());
+		return true;
 	}
 }
