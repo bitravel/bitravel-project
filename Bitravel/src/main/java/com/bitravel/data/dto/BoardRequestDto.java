31f32a97dc36854d3034dcb21cd8f0f4239020e1
spacekay <bethjkim0311@gmail.com>
2021-11-17 16:29:50 +0900

    deleted unused(not effective) method

--- Bitravel/src/main/java/com/bitravel/data/dto/BoardRequestDto.java
+++ Bitravel/src/main/java/com/bitravel/data/dto/BoardRequestDto.java
@@ -14,13 +14,15 @@ public class BoardRequestDto {
 	private String userEmail; // 작성자 (이메일)
     private String boardTitle; // 제목
     private String boardContent; // 내용
-
+    private Integer boardLevel; // 글 레벨 (답글 여부 확인)
+    
+    
     public Board toEntity() {
         return Board.builder()
                 .boardTitle(boardTitle)
                 .boardContent(boardContent)
                 .userEmail(userEmail)
-                .boardView(0)
+                .boardLevel(boardLevel)
                 .build();
     }
 
