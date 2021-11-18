644ab7cc07358a45a4f5cc1c059c033db2142d06
spacekay <bethjkim0311@gmail.com>
2021-11-18 10:36:49 +0900

    Review Entity의 기본 동작 관련 classes

---
+++ Bitravel/src/main/java/com/bitravel/controller/ReviewController.java
@@ -0,0 +1,75 @@
+package com.bitravel.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bitravel.data.dto.ReviewRequestDto;
+import com.bitravel.data.dto.ReviewResponseDto;
+import com.bitravel.data.entity.Review;
+import com.bitravel.service.ReviewService;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+@Api(value = "ReviewController")
+public class ReviewController {
+
+	// 단순 게시판에 비하여 여행지 등록 및 불러오기 관련 기능이 추후 추가될 수 있음
+    private final ReviewService reviewService;
+
+    /**
+     * 게시글 작성
+     */
+    @PostMapping("/reviews")
+    @ApiOperation(value = "글 작성", notes = "글 내용을 저장하는 API. Review entity 클래스로 데이터를 저장한다.")
+    public Long save(@RequestBody final ReviewRequestDto params) {
+        return reviewService.save(params);
+    }
+
+    /**
+     * 게시글 리스트 조회
+     */
+    @GetMapping("/reviews")
+    @ApiOperation(value = "글 목록 조회", notes = "글 목록을 조회하는 API.")
+    public List<ReviewResponseDto> findAll() {
+        return reviewService.findAll();
+    }
+
+    /**
+     * 게시글 수정
+     */
+    @PatchMapping("/reviews/{id}")
+    @ApiOperation(value = "글 수정", notes = "글 내용을 수정하는 API. Review entity 클래스로 데이터를 수정한다.<br>이때엔 정보를 등록할 때와는 다르게 bid 값을 함깨 보내줘야한다.")
+    public Boolean save(@PathVariable final Long id, @RequestBody final ReviewRequestDto params) {
+        return reviewService.update(id, params);
+    }
+    /**
+     * 게시글 상세 정보 조회
+     */
+    @GetMapping("/reviews/{id}")
+    @ApiOperation(value = "글 내용 조회", notes = "개별 글의 정보를 조회하는 API. Review entity 클래스의 bid값을 기준으로 데이터를 가져온다.")
+    public Review detail(@PathVariable final Long id) {
+    	return reviewService.detail(id);
+    }
+    /**
+     * 게시글 삭제
+     */
+    @DeleteMapping("/reviews/{id}")
+    @ApiOperation(value = "글 삭제", notes = "글 내용을 삭제하는 API. Review entity 클래스의 bid 값으로 데이터를 삭제한다.")
+    public Boolean deleteById(@PathVariable Long id) {	
+    	return reviewService.deleteById(id);
+    }
+    
+}
---
+++ Bitravel/src/main/java/com/bitravel/service/ReviewService.java
@@ -0,0 +1,90 @@
+package com.bitravel.service;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bitravel.data.dto.ReviewRequestDto;
+import com.bitravel.data.dto.ReviewResponseDto;
+import com.bitravel.data.entity.Review;
+import com.bitravel.data.repository.ReviewRepository;
+import com.bitravel.exception.CustomException;
+import com.bitravel.exception.ErrorCode;
+import com.bitravel.util.SecurityUtil;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ReviewService {
+	private final ReviewRepository reviewRepository;
+	
+    /**
+     * 게시글 생성
+     */
+    @Transactional
+    public Long save(ReviewRequestDto params) {
+    	// JWT 구현 전에는 anonymousUser로 기록됨
+    	params.setUserEmail(SecurityUtil.getCurrentEmail().get());
+        Review entity = reviewRepository.save(params.toEntity());
+        return entity.getReviewId();
+    }
+
+    /**
+     * 게시글 리스트 조회
+     */
+    public List<ReviewResponseDto> findAll() {
+
+        Sort sort = Sort.by(Direction.DESC, "reviewId", "reviewDate");
+        List<Review> list = reviewRepository.findAll(sort);
+        return list.stream().map(ReviewResponseDto::new).collect(Collectors.toList());
+    }
+    
+    /**
+     * 게시글 상세 정보 조회
+     */
+    @Transactional(readOnly = true)
+    public Review detail(Long id) {
+    	return reviewRepository.findById(id).orElseThrow(() -> new CustomException(ErrorCode.POSTS_NOT_FOUND));
+    }
+
+    /**
+     * 게시글 수정
+     */
+    @Transactional
+    public Boolean update(final Long id, final ReviewRequestDto params) {
+        Review entity = reviewRepository.findById(id).orElseThrow(() -> new CustomException(ErrorCode.POSTS_NOT_FOUND));
+        // 글쓴이 또는 admin만 수정할 수 있게 함
+        if(SecurityUtil.getCurrentEmail().get().equals("admin")) {
+        	log.info("관리자 권한으로 글을 수정합니다. 글 번호 : "+id);
+        } else if(!entity.getUserEmail().equals(SecurityUtil.getCurrentEmail().get())) {
+        	log.info("유효하지 않은 수정 요청입니다.");
+        	return false;
+        }
+        entity.update(params.getReviewTitle(), params.getReviewContent());
+        return true;
+    }
+    
+    /**
+     * 게시글 삭제
+     */
+    @Transactional
+    public Boolean deleteById(Long id) {
+        Review entity = reviewRepository.findById(id).orElseThrow(() -> new CustomException(ErrorCode.POSTS_NOT_FOUND));
+        // 글쓴이 또는 admin만 수정할 수 있게 함
+        if(SecurityUtil.getCurrentEmail().get().equals("admin")) {
+        	log.info("관리자 권한으로 글을 삭제합니다. 글 번호 : "+id);
+        } else if(!entity.getUserEmail().equals(SecurityUtil.getCurrentEmail().get())) {
+        	log.info("유효하지 않은 삭제 요청입니다.");
+        	return false;
+        }
+    	reviewRepository.deleteById(id);
+    	return true;
+    }
+    
+}
