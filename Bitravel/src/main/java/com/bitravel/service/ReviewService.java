835191bb8bff470f86e05be6770786f7a6180934
spacekay <bethjkim0311@gmail.com>
2021-11-18 17:42:06 +0900

    게시물 -> 후기로 수정

--- Bitravel/src/main/java/com/bitravel/service/ReviewService.java
+++ Bitravel/src/main/java/com/bitravel/service/ReviewService.java
@@ -29,25 +29,25 @@ public class ReviewService {
 	private final ReviewRepository reviewRepository;
 	private final TravelRepository travelRepository;
     /**
-     * 게시글 생성
+     * 후기 생성
      */
     @Transactional
     public Long save(ReviewRequestDto params) {
     	// JWT 구현 전에는 anonymousUser로 기록됨
     	params.setUserEmail(SecurityUtil.getCurrentEmail().get());
-    	Set<Travel> travelList = new HashSet<>();
+    	Set<Travel> travelSet = new HashSet<>();
     	List<Long> travelIds = params.getTravelId();
     	int L = travelIds.size();
     	for(int i=0; i<L; i++) {
-    		travelList.add(travelRepository.getById(travelIds.get(i)));
+    		travelSet.add(travelRepository.getById(travelIds.get(i)));
     	}
-    	params.setTravel(travelList);
+    	params.setTravel(travelSet);
         Review entity = reviewRepository.save(params.toEntity());
         return entity.getReviewId();
     }
 
     /**
-     * 게시글 리스트 조회
+     * 후기 리스트 조회
      */
     public List<ReviewResponseDto> findAll() {
 
@@ -57,15 +57,15 @@ public class ReviewService {
     }
     
     /**
-     * 게시글 상세 정보 조회
+     * 후기 상세 정보 조회
      */
     @Transactional(readOnly = true)
     public Review detail(Long id) {
-    	return reviewRepository.findById(id).orElseThrow(() -> new CustomException(ErrorCode.POSTS_NOT_FOUND));
+    	return reviewRepository.findById(id).orElseThrow(() -> new CustomException(ErrorCode.POSTS_NOT_FOUND)); 
     }
 
     /**
-     * 게시글 수정
+     * 후기 수정
      */
     @Transactional
     public Boolean update(final Long id, final ReviewRequestDto params) {
@@ -77,18 +77,18 @@ public class ReviewService {
         	log.info("유효하지 않은 수정 요청입니다.");
         	return false;
         }
-    	Set<Travel> travelList = new HashSet<>();
+    	Set<Travel> travelSet = new HashSet<>();
     	List<Long> travelIds = params.getTravelId();
     	int L = travelIds.size();
     	for(int i=0; i<L; i++) {
-    		travelList.add(travelRepository.getById(travelIds.get(i)));
+    		travelSet.add(travelRepository.getById(travelIds.get(i)));
     	}
-        entity.update(params.getReviewTitle(), params.getReviewContent(), travelList);
+        entity.update(params.getReviewTitle(), params.getReviewContent(), travelSet);
         return true;
     }
     
     /**
-     * 게시글 삭제
+     * 후기 삭제
      */
     @Transactional
     public Boolean deleteById(Long id) {
