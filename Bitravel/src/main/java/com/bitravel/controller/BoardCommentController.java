90a0a4925d500e8ec861540bfa99a15ba0bba3d8
spacekay <bethjkim0311@gmail.com>
2021-11-18 15:25:11 +0900

    url pattern 안 겹치게 변경

--- Bitravel/src/main/java/com/bitravel/controller/BoardCommentController.java
+++ Bitravel/src/main/java/com/bitravel/controller/BoardCommentController.java
@@ -31,7 +31,7 @@ public class BoardCommentController {
     /**
      * 특정 게시글의 댓글 모두 보기
      */
-    @GetMapping("/comments/{id}")
+    @GetMapping("/boards/comments/{id}")
     @ApiOperation(value = "댓글 조회", notes = "parameter로 주어진 boardId에 해당하는 댓글을 리스트로 모두 호출한다.")
     public List<BoardCommentResponseDto> findAllComments(@PathVariable final Long id) {
         return bCommentService.findAllComments(id);
@@ -40,7 +40,7 @@ public class BoardCommentController {
     /**
      * 특정 게시물에 댓글 작성
      */
-    @PostMapping("/comments/{id}")
+    @PostMapping("/boards/comments/{id}")
     @ApiOperation(value = "댓글 작성", notes = "댓글 내용을 저장하는 API. BoardComment entity 클래스로 데이터를 저장한다.")
     public Long save(@RequestBody final BoardCommentRequestDto params) {    	
     	return bCommentService.saveComment(params);
@@ -49,7 +49,7 @@ public class BoardCommentController {
     /**
      * 특정 게시물에 댓글 수정 (board id은 hidden input tag 등으로 같이 전달되도록 해야 함)
      */
-    @PatchMapping("/comments/{id}")
+    @PatchMapping("/boards/comments/{id}")
     @ApiOperation(value = "댓글 수정", notes = "댓글 내용을 수정하는 API. BoardComment entity 클래스로 데이터를 수정한다.<br>이때엔 정보를 등록할 때와는 다르게 cid 값을 함깨 보내줘야한다.")
     public Boolean save(@PathVariable final Long id, @RequestBody final BoardCommentRequestDto params) {
         return bCommentService.update(id, params);
@@ -58,7 +58,7 @@ public class BoardCommentController {
     /**
      * 특정 게시물에 댓글 삭제
      */
-    @DeleteMapping("/comments/{id}")
+    @DeleteMapping("/boards/comments/{id}")
     @ApiOperation(value = "댓글 삭제", notes = "댓글 내용을 삭제하는 API. BoardComment entity 클래스의 id 값으로 데이터를 삭제한다.")
     public Boolean deleteById(@PathVariable Long id) {	
     	return bCommentService.deleteById(id);
--- Bitravel/src/main/java/com/bitravel/controller/ReviewCommentController.java
+++ Bitravel/src/main/java/com/bitravel/controller/ReviewCommentController.java
@@ -31,7 +31,7 @@ public class ReviewCommentController {
     /**
      * 특정 게시글의 댓글 모두 보기
      */
-    @GetMapping("/comments/{id}")
+    @GetMapping("/reviews/comments/{id}")
     @ApiOperation(value = "댓글 조회", notes = "parameter로 주어진 reviewId에 해당하는 댓글을 리스트로 모두 호출한다.")
     public List<ReviewCommentResponseDto> findAllComments(@PathVariable final Long id) {
         return rCommentService.findAllComments(id);
@@ -40,7 +40,7 @@ public class ReviewCommentController {
     /**
      * 특정 게시물에 댓글 작성
      */
-    @PostMapping("/comments/{id}")
+    @PostMapping("/reviews/comments/{id}")
     @ApiOperation(value = "댓글 작성", notes = "댓글 내용을 저장하는 API. ReviewComment entity 클래스로 데이터를 저장한다.")
     public Long save(@RequestBody final ReviewCommentRequestDto params) {    	
     	return rCommentService.saveComment(params);
@@ -49,7 +49,7 @@ public class ReviewCommentController {
     /**
      * 특정 게시물에 댓글 수정 (review id은 hidden input tag 등으로 같이 전달되도록 해야 함)
      */
-    @PatchMapping("/comments/{id}")
+    @PatchMapping("/reviews/comments/{id}")
     @ApiOperation(value = "글 수정", notes = "댓글 내용을 수정하는 API. ReviewComment entity 클래스로 데이터를 수정한다.<br>이때엔 정보를 등록할 때와는 다르게 cid 값을 함깨 보내줘야한다.")
     public Boolean save(@PathVariable final Long id, @RequestBody final ReviewCommentRequestDto params) {
         return rCommentService.update(id, params);
@@ -58,7 +58,7 @@ public class ReviewCommentController {
     /**
      * 특정 게시물에 댓글 삭제
      */
-    @DeleteMapping("/comments/{id}")
+    @DeleteMapping("/reviews/comments/{id}")
     @ApiOperation(value = "글 삭제", notes = "댓글 내용을 삭제하는 API. ReviewComment entity 클래스의 id 값으로 데이터를 삭제한다.")
     public Boolean deleteById(@PathVariable Long id) {	
     	return rCommentService.deleteById(id);
