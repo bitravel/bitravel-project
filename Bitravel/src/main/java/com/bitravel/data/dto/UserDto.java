885fd3ed0b72d2678b05ba1a858e162c570d96cd
spacekay <bethjkim0311@gmail.com>
2021-11-18 17:39:35 +0900

    Size Annotation 삭제 (가급적 Frontend에서 검증)

--- Bitravel/src/main/java/com/bitravel/data/dto/UserDto.java
+++ Bitravel/src/main/java/com/bitravel/data/dto/UserDto.java
@@ -1,7 +1,6 @@
 package com.bitravel.data.dto;
 
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 import com.bitravel.data.entity.User;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -23,20 +22,16 @@ public class UserDto {
 	private Long userId;
 	
 	@NotNull
-	@Size(min=5, max=50)
 	private String email;
 	
 	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
 	@NotNull
-    @Size(min=4, max=100)
 	private String password; // 회원가입 시에만 사용함. 그 외에는 외부로 유출되지 않음
 	
 	@NotNull
-	@Size(min=2, max=20)
 	private String nickname;
 	
 	@NotNull
-	@Size(min=2, max=20)
 	private String realname;
 	
 	@NotNull
