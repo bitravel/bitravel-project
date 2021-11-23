866edb644f0aa66530677e59253424380b6105a2
sisi <djdkek@gmail.com>
2021-11-23 20:30:11 +0900

    게시글 상세페이지 맵핑

--- Bitravel/src/main/java/com/bitravel/controller/BoardPageController.java
+++ Bitravel/src/main/java/com/bitravel/controller/BoardPageController.java
@@ -1,8 +1,11 @@
 package com.bitravel.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/board")
@@ -20,8 +23,18 @@ public class BoardPageController {
      * 게시글 등록 페이지
      */
     @GetMapping("/write")
-    public String openBoardWrite() {
+    public String openBoardWrite(@RequestParam(required = false) final Long id, Model model) {
+        model.addAttribute("id", id);
         return "board/write";
+    }
+    
+    /**
+     * 게시글 상세 페이지
+     */
+    @GetMapping("/detail/{id}")
+    public String openDetailWriting(@PathVariable final Long id, Model model) {
+        model.addAttribute("id", id);  //model을 통해서 id값 넣어줌
+        return "board/detail";
     }
 
 }
