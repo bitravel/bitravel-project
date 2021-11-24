180d3d152143a8e3e31c63c15f808f49d9de9991
spacekay <bethjkim0311@gmail.com>
2021-11-24 15:05:31 +0900

    회원가입 시 선호 지역 등록 관련 필요한 class들

---
+++ Bitravel/src/main/java/com/bitravel/controller/RegionController.java
@@ -0,0 +1,86 @@
+package com.bitravel.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bitravel.data.entity.Region;
+import com.bitravel.data.entity.UserRegion;
+import com.bitravel.service.RegionService;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+@Api(value = "RegionController")
+public class RegionController {
+    
+    private final RegionService regionService;
+    
+    /**
+     * 회원별 선호 지역 추가
+     */
+    @PostMapping("/regions")
+    @ApiOperation(value = "회원별 선호 지역 작성", notes = "회원별 선호 지역 내용을 저장하는 API. UserRegion entity 클래스로 데이터를 저장한다.")
+    public Long save(@RequestBody final UserRegion params) {
+        return regionService.save(params);
+    }
+
+    /**
+     * 회원별 선호 지역 리스트 조회
+     */
+    @GetMapping("/regions/favorite")
+    @ApiOperation(value = "회원별 선호 지역 조회", notes = "회원별 선호 지역을 조회하는 API.")
+    public List<UserRegion> findAllByUser(@Valid String userEmail) {
+        return regionService.findAllByUser(userEmail);
+    }
+    
+    /**
+     *  나의 선호 지역 리스트 조회
+     */
+    @GetMapping("/regions/favorite/mylist")
+    @ApiOperation(value = "나의 선호 지역 조회", notes = "현재 로그인한 회원의 선호 지역을 조회하는 API.")
+    public List<UserRegion> findAllByMe() {
+    	// 추후 비회원인 경우 해당 서비스 접근 제한 예정
+        return regionService.findAllByMe();
+    }
+      
+    /**
+     *  광역자치단체별 기초자치단체 검색
+     */
+    @GetMapping("/regions/list/{name}")
+    @ApiOperation(value = "광역자치단체 목록 검색", notes = "광역자치단체별 기초자치단체 목록을 출력하는 API. Region entity 클래스의 LargeGov값을 기준으로 데이터를 가져온다.")
+    public List<Region> ListByLargeGov(@PathVariable final String name) {
+    	return regionService.ListByLargeGov(name);
+    }
+    
+    /**
+     * 전체 기초자치단체 검색
+     */
+    @GetMapping("/regions/list")
+    @ApiOperation(value = "기초자치단체 목록 검색", notes = "전체 기초자치단체 목록을 출력하는 API. Region entity 클래스의 모든 데이터를 가져온다.")
+    public List<Region> ListAll() {
+    	return regionService.ListAll();
+    }
+    
+    /**
+     * 나의 선호 지역 삭제
+     */
+    @DeleteMapping("/regions/{id}")
+    @ApiOperation(value = "회원별 선호 지역 삭제", notes = "회원별 선호 지역 내용을 삭제하는 API. UserRegion entity 클래스의 id 값으로 데이터를 삭제한다.")
+    public Boolean deleteById(@PathVariable Long id) {	
+    	return regionService.deleteById(id);
+    }
+    
+}
---
+++ Bitravel/src/main/java/com/bitravel/service/RegionService.java
@@ -0,0 +1,89 @@
+package com.bitravel.service;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bitravel.data.entity.Region;
+import com.bitravel.data.entity.UserRegion;
+import com.bitravel.data.repository.RegionRepository;
+import com.bitravel.data.repository.UserRegionRepository;
+import com.bitravel.exception.CustomException;
+import com.bitravel.exception.ErrorCode;
+import com.bitravel.util.SecurityUtil;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class RegionService {
+
+	private final UserRegionRepository userRegionRepository;
+	private final RegionRepository regionRepository;
+
+	/**
+	 * 회원별 선호 지역 추가
+	 */
+	@Transactional
+	public Long save(UserRegion params) {
+		params.setUserEmail(SecurityUtil.getCurrentEmail().get());
+		UserRegion entity = userRegionRepository.save(params);
+		return entity.getUserRegionId();
+	}
+
+	/**
+	 * 회원별 선호 지역 리스트 조회
+	 */
+	public List<UserRegion> findAllByUser(String keyword) {
+		return userRegionRepository.findByUserEmail(keyword);
+	}
+
+	/**
+	 * 나의 선호 지역 리스트 조회
+	 */
+	public List<UserRegion> findAllByMe() {
+		String keyword = SecurityUtil.getCurrentEmail().get();
+		return userRegionRepository.findByUserEmail(keyword);
+	}
+
+
+	/**
+	 * 광역자치단체별 기초자치단체 검색
+	 */
+	@Transactional(readOnly = true)
+	public List<Region> ListByLargeGov(String largeGov) {
+		List<Region> list = regionRepository.findByLargeGov(largeGov);
+		return list;
+	}
+
+	/**
+	 * 전체 기초자치단체 검색
+	 */
+	@Transactional(readOnly = true)
+	public List<Region> ListAll() {
+		List<Region> list = regionRepository.findAll();
+		return list;
+	}
+
+	/**
+	 * 나의 선호 지역 삭제
+	 */
+	@Transactional
+	public Boolean deleteById(Long id) {
+		UserRegion userRegion = userRegionRepository.findById(id).orElseThrow(() -> new CustomException(ErrorCode.POSTS_NOT_FOUND)); 
+		// 글쓴이 또는 admin만 수정할 수 있게 함
+		if(SecurityUtil.getCurrentEmail().get().equals("admin")) {
+			log.info("관리자 권한으로 글을 삭제합니다. 글 번호 : "+id);
+		} else if (SecurityUtil.getCurrentEmail().get().equals(userRegion.getUserEmail())) {
+			log.info(userRegion.getUserEmail()+ "회원이 본인의 선호 지역을 삭제합니다. 글 번호 : "+id);
+		} else {
+			log.info("유효하지 않은 삭제 요청입니다.");
+			return false;
+		}
+		userRegionRepository.deleteById(id);
+		return true;
+	}
+
+}
