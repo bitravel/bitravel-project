f7a32c5689ec984ea40a615786dc673023e44391
sisi <djdkek@gmail.com>
2021-11-26 17:38:12 +0900

    json으로 받는 방식에서 model에 담아서 받는 방식으로 수정

--- Bitravel/src/main/java/com/bitravel/controller/BoardPageController.java
+++ Bitravel/src/main/java/com/bitravel/controller/BoardPageController.java
@@ -1,5 +1,8 @@
 package com.bitravel.controller;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,16 +10,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.bitravel.service.BoardService;
+
+import lombok.RequiredArgsConstructor;
+
 @Controller
 @RequestMapping("/board")
+@RequiredArgsConstructor
 public class BoardPageController {
-
+	
+	private final BoardService boardService;
+	
     /**
      * 게시글 리스트 페이지
      */
     @GetMapping("/list")
-    public String openBoardList() {
-        return "board/board";
+    public String board(Model model, @PageableDefault(size = 10, sort = "boardId", direction = Sort.Direction.DESC) Pageable pageable) {
+        
+    	model.addAttribute("boardList", boardService.findAll(pageable));
+    	
+    	return "board/board";
     }
 
     /**
