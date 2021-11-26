93c2d25a09ca990a5da206b83bbe89cd2dfffab1
spacekay <bethjkim0311@gmail.com>
2021-11-26 13:15:42 +0900

    User-Travel 간 관계 설정 table 작성

---
+++ Bitravel/src/main/java/com/bitravel/data/entity/UserTravel.java
@@ -0,0 +1,50 @@
+package com.bitravel.data.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "userTravel")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserTravel {
+	 
+	// 유저별 관심 지역을 저장하는 Entity
+
+	@JsonProperty(access=JsonProperty.Access.READ_ONLY)
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "userRegionId")
+	private Long userTravelId;
+	
+	@Column(name = "userEmail")
+	private String userEmail;
+	
+	@Column(name = "travelId")
+	private Long travelId;
+	
+	// 가입 시에는 방문여부는 다 FALSE, 선호 여부는 선택에 따라 다르게
+    @Column(name = "isVisited", columnDefinition = "BIT(1) DEFAULT 0")
+    private boolean isVisited; // 방문 여부
+    
+    @Column(name = "isLiked", columnDefinition = "BIT(1) DEFAULT 0")
+    private boolean isLiked; // 선호 여부
+    
+    @Column(name = "isWishlisted", columnDefinition = "BIT(1) DEFAULT 0")
+    private boolean isWishlisted; // 여행지 찜 여부
+	
+}
