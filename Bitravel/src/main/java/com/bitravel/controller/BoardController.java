9aa3baf2d307fad7ebbc8b0487fad8118a8f80ed
spacekay <bethjkim0311@gmail.com>
2021-12-06 15:57:12 +0900

    불필요한 import 제거

--- Bitravel/src/main/java/com/bitravel/controller/BoardController.java
+++ Bitravel/src/main/java/com/bitravel/controller/BoardController.java
@@ -1,8 +1,5 @@
 package com.bitravel.controller;
 
-import java.util.List;
-
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -10,12 +7,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bitravel.data.dto.BoardRequestDto;
 import com.bitravel.data.dto.BoardResponseDto;
-import com.bitravel.data.entity.Board;
 import com.bitravel.service.BoardService;
 
 import io.swagger.annotations.Api;
