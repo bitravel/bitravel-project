a64f7bb06453161b631dbc1cdbbd056c1ccdb55d
sisi <djdkek@gmail.com>
2021-12-07 21:15:27 +0900

    리뷰 작성 페이지 구현

--- Bitravel/src/main/java/com/bitravel/controller/ReviewPageController.java
+++ Bitravel/src/main/java/com/bitravel/controller/ReviewPageController.java
@@ -3,6 +3,7 @@ package com.bitravel.controller;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -44,7 +45,7 @@ public class ReviewPageController {
      * 후기 상세 페이지
      */
     @GetMapping("/detail/{id}")
-    public String openDetailWriting(@PathVariable final Long id, Model model, @RequestParam(value = "page") final Long page) {
+    public String openDetailWriting(@PathVariable final Long id, Model model, @RequestParam(value = "page") @Nullable final Long page) {
         model.addAttribute("id", id);  //model을 통해서 id값 넣어줌
         
         return "review/reviewDetail";
