dbe39978ebaae52025bda129471cd5c13a557934
spacekay <bethjkim0311@gmail.com>
2021-12-08 11:51:55 +0900

    html - js 분리 및 reformatting

---
+++ Bitravel/src/main/resources/static/js/bitravel/boardList.js
@@ -0,0 +1,24 @@
+	/*<![CDATA[*/
+		
+		 /**
+		 * 게시글 조회
+		 */
+		function goView(id) {
+			location.href = `/board/detail/${id}`;
+		}
+		/**
+		* 검색 옵션 변경
+		**/
+		function option_bt(act) { 
+		    if (act=='all'){
+		    	document.edit.action = "/board/search/all/list";
+		    } else if (act=='title') {
+		    	document.edit.action = "/board/search/title/list";
+		    } else if (act=='titleandcontent') {
+		    	document.edit.action = "/board/search/titleandcontent/list";
+		    } else {
+		    	document.edit.action = "/board/search/nickname/list";
+		    }
+		}
+
+	/*]]>*/
