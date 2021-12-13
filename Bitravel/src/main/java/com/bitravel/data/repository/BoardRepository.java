076be9ce0b1f7f1da373e5416c7eadf097d1c811
spacekay <bethjkim0311@gmail.com>
2021-12-13 20:52:34 +0900

    게시판 검색 기능 보강 + 포인트 기능 연결

--- Bitravel/src/main/java/com/bitravel/data/repository/BoardRepository.java
+++ Bitravel/src/main/java/com/bitravel/data/repository/BoardRepository.java
@@ -1,9 +1,11 @@
 package com.bitravel.data.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bitravel.data.entity.Board;
@@ -19,4 +21,7 @@ public interface BoardRepository extends JpaRepository<Board, Long>{
 	Page<Board> findByBoardTitleContaining(String keyword, Pageable pageable);
 	
 	Page<Board> findByBoardTitleContainingOrBoardContentContaining(String boardTitle, String boardContent, Pageable pageable);
+
+	List<Board> findByNicknameContainingOrBoardTitleContainingOrBoardContentContaining(String nickname, String boardTitle,
+			String boardContent, Sort sort);
 }
