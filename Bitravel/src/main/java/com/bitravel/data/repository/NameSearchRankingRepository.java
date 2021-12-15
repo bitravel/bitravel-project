acac8dc7cb5275e45e3072b009de5b924124b46f
spacekay <bethjkim0311@gmail.com>
2021-12-15 14:00:12 +0900

    made non-usable entity deprecated

--- Bitravel/src/main/java/com/bitravel/data/repository/NameSearchRankingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bitravel.data.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bitravel.data.entity.NameSearchRanking;
-
-// Entity가 하나 추가될 때마다 별개의 Repository class가 필요함
-public interface NameSearchRankingRepository extends JpaRepository<NameSearchRanking, Long>{
-}
