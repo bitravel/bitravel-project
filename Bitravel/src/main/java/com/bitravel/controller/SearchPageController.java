a1239f0c51b5562f78162c6d63b4e9bceb874fad
spacekay <bethjkim0311@gmail.com>
2021-12-16 18:38:48 +0900

    미사용 annotation 삭제

--- Bitravel/src/main/java/com/bitravel/controller/SearchPageController.java
+++ Bitravel/src/main/java/com/bitravel/controller/SearchPageController.java
@@ -25,9 +25,7 @@ import com.bitravel.service.UserService;
 import com.bitravel.util.TagUtil;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class SearchPageController {
