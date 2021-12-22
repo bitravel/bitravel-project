67e27747ce004ffa34ae82a08b0527777655b78d
spacekay <bethjkim0311@gmail.com>
2021-12-22 15:23:30 +0900

    태그 제거 기능 필요한 DTO에 TagUtil 적용

--- Bitravel/src/main/java/com/bitravel/data/dto/BoardCommentRequestDto.java
+++ Bitravel/src/main/java/com/bitravel/data/dto/BoardCommentRequestDto.java
@@ -2,6 +2,7 @@ package com.bitravel.data.dto;
 
 import com.bitravel.data.entity.Board;
 import com.bitravel.data.entity.BoardComment;
+import com.bitravel.util.TagUtil;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AccessLevel;
@@ -26,7 +27,7 @@ public class BoardCommentRequestDto {
 	public BoardComment toEntity() {
 		return BoardComment.builder()
 				.board(board)
-				.commentContent(commentContent)
+				.commentContent(TagUtil.getText(commentContent))
 				.userEmail(userEmail)
 				.nickname(nickname)
 				//.commentLevel(commentLevel)
